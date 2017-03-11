b535b41b59de96940bc312bc717a6e4e086c4247
kedar nadh reddy <kedarreddy143@gmail.com>
2017-03-11 10:13:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+function myFunction() {
+    document.getElementById("dem").innerHTML = " <xmp>       <input type='button' class='button' id='id' value=' button' onclick='myFunction1()''> </xmp>  ";
+
+}
+function myFunction6() {
+    document.getElementById("dem6").innerHTML = " <code> <pre>  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp.button { background-color: #4CAF50; color: white;  padding: 15px 32px; text-align: center;font-size: 16px; margin: 4px 2px;</pre> </code>";
+}
+function myFunction10() {
+    document.getElementById("dem8").innerHTML = " <code> <input type='button' class='button' id='id' value='CSS button' > </code>  ";
+}
+function myFunction1() {
+    document.getElementById("dem1").innerHTML = " <xmp>        <button>button</button> </xmp> ";
+}
+function myFunction9() {
+    document.getElementById("dem7").innerHTML = " <code> <button>button</button> </code> ";
+}
+function myFunction16() {
+    document.getElementById("dem18").innerHTML = " <xmp>       <marquee>Enter text to marquee</marquee> </xmp> ";
+}
+function myFunction17() {
+    document.getElementById("dem17").innerHTML = " <code> <marquee>Enter text to marquee</marquee></code> ";
+}
+
+function myFunction2() {
+    document.getElementById("dem2").innerHTML = " <xmp>   <!--comment here--> </xmp> ";
+}
+
+function myFunction3() {
+    document.getElementById("dem3").innerHTML = " <xmp>     <form> </xmp> ";
+}
+function myFunction7() {
+    document.getElementById("dem4").innerHTML = " <xmp>     </form> </xmp> ";
+}
+function myFunction4() {
+    document.getElementById("dem9").innerHTML = " <xmp>       <img src='code.png' alt='img.png' > </xmp> ";
+}
+function myFunction11() {
+    document.getElementById("dem10").innerHTML = " <code><img src='code.png' alt='img.png'></code> ";
+}
+function myFunction5() {
+    document.getElementById("dem11").innerHTML = " <xmp>      <p>Text here</p> </xmp> ";
+}
+function myFunction13() {
+    document.getElementById("dem13").innerHTML = " <code><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat tellus neque, ut tristique nunc bibendLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat tellus neque, ut tristique nunc bibendum id. In orci leo, porttitor non ex ut, lacinia rhoncus justo. Pellentesque nulla elit, luctus eu leo sit amet, tincidunt fringilla ligula. In ac ultrices tellus. Curabitur lacus enim, fringilla et leo sit amet, posuere fringilla risus. Nulla dapibus nulla nulla, a cursus velit tempor sit amet. Donec rhoncus arcu quam, ut fringilla lacus laoreet ac. Ut sit amet urna lacus.</p> </code> ";
+}
+function myFunction8() {
+    document.getElementById("dem5").innerHTML = "<xmp>         <input type='text' value='text' id='text'> </xmp> ";
+}
+function myFunction12() {
+    document.getElementById("dem12").innerHTML = "<code><input type='text' value='text' id='text'> </code> ";
+}
+function myFunction14() {
+    document.getElementById("dem14").innerHTML = " <xmp>       <h1>Your HEADING here</h2> </xmp> ";
+}
+function myFunction15() {
+    document.getElementById("dem15").innerHTML = " <code><h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat tellus neque, ut tristique nunc bibendLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat tellus neque, ut tristique nunc bibendum id. In orci leo, porttitor non ex ut, lacinia rhoncus justo. Pellentesque nulla elit, luctus eu leo sit amet, tincidunt fringilla ligula. In ac ultrices tellus. Curabitur lacus enim, fringilla et leo sit amet, posuere fringilla risus. Nulla dapibus nulla nulla, a cursus velit tempor sit amet. Donec rhoncus arcu quam, ut fringilla lacus laoreet ac. Ut sit amet </h1> </code> ";
+}
